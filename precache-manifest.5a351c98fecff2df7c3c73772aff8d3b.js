14ec6f01440e8eedfd55640486a789004f7a412e
Claire Filipek <cfilipek@sva.edu>
2019-06-29 07:10:05 -0400

    Updates

---
+++ precache-manifest.5a351c98fecff2df7c3c73772aff8d3b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07ebef45fa12150076874009585b9e85",
+    "url": "/GH-countdown-clock/index.html"
+  },
+  {
+    "revision": "39f597544cb3c33948ba",
+    "url": "/GH-countdown-clock/static/css/main.32135bd7.chunk.css"
+  },
+  {
+    "revision": "1fd3df0568f731e02652",
+    "url": "/GH-countdown-clock/static/js/2.7196c04f.chunk.js"
+  },
+  {
+    "revision": "39f597544cb3c33948ba",
+    "url": "/GH-countdown-clock/static/js/main.37b8e52a.chunk.js"
+  },
+  {
+    "revision": "777967236f047697bf9f",
+    "url": "/GH-countdown-clock/static/js/runtime~main.b3fab60e.js"
+  }
+]);
